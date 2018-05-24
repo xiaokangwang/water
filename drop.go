3e9597577724c833e92174d80f1b4450a601c25a
Shelikhoo <xiaokangwang@outlook.com>
2018-05-24 10:25:35 +0800

    Added Drop

--- drop.go
+++ drop.go
@@ -2,6 +2,6 @@ package water
 
 import "os"
 
-func DropTAPInterface(fd os.File) *Interface {
-	return &Interface{isTAP: true, name: "/dev/net/tun", fd: int(fd.Fd()), ReadWriteCloser: &fd}
+func DropTunInterface(fd os.File) *Interface {
+	return &Interface{isTAP: false, name: "/dev/net/tun", fd: int(fd.Fd()), ReadWriteCloser: &fd}
 }
